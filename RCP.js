2ab816a9b5b726658fe0af170ce846f991f69dff
Pierre SEIN <pierre@ehda.co>
2017-08-03 11:05:56 +0200

    Fixed the name of the functions

--- RCP.js
+++ RCP.js
@@ -1,5 +1,3 @@
-console.log("Coucou");
-
 
 // This function generates the right dat
 function generate(){
@@ -13,16 +11,20 @@ function sendTransaction(data, callback){
 	var url = "http://127.0.0.1:8545";
 	var params = data;
 	http.open("POST", url, true);
-
+	
 	//Send the proper header information along with the request
 	//http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-	http.onreadystatechange = function() {//Call a function when the state changes.
-	    if(http.readyState == 4 && http.status == 200) {
-	        alert(http.responseText);
-	    }}
+	http.onreadystatechange = callback(http);
 	
 	http.send(params);
 }
 
-sendTransaction(produceData(),null)
+sendTransaction(generate(),function(http) {//Call a function when the state changes.
+	this.http=http
+	console.log(this.http.readyState)
+	console.log(this.http.status)
+	
+    if(http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+    }})
